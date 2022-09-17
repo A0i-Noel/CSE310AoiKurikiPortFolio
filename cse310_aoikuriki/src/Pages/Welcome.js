6f39558eeb21b937d5a228df9dcd973862e45a5f
A0i-Noel <91494260+A0i-Noel@users.noreply.github.com>
2022-09-17 15:44:05 -0600

    Hello World

---
+++ cse310_aoikuriki/src/Pages/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div className='welcomeContainer'>
+      <h2>CSE310 Apply Programming</h2>
+      <h3>Aoi Kuriki</h3>
+      <h1> Hello World</h1>
+    </div>
+  )
+}
+
+export default Welcome
